4d9ce8f2d2996b10fc1b2fbbfbbd6274408a0537
Joe Audette <joeaudette@users.noreply.github.com>
2015-02-05 15:58:17 -0500

    wip on nuget packaging

--- nuget/cloudscribe.Core.Web.Integration.cmd
+++ nuget/cloudscribe.Core.Web.Integration.cmd
@@ -10,26 +10,81 @@ mkdir cloudscribe.Core.Web.Integration\content\Views
 mkdir cloudscribe.Core.Web.Integration\content\DI
 mkdir cloudscribe.Core.Web.Integration\content\DI\Autofac
 
-
-xcopy ..\src\cloudscribe.WebHost\Startup.cs cloudscribe.Core.Web.Integration\content /y
 xcopy ..\src\cloudscribe.WebHost\Web.AppSettings.config.sample cloudscribe.Core.Web.Integration\content /y
 xcopy ..\src\cloudscribe.WebHost\site.sitemap cloudscribe.Core.Web.Integration\content /y
-xcopy ..\src\cloudscribe.WebHost\Global.asax cloudscribe.Core.Web.Integration\content /y
-xcopy ..\src\cloudscribe.WebHost\Global.asax.cs cloudscribe.Core.Web.Integration\content /y
-
-
-
 xcopy ..\src\cloudscribe.WebHost\log4net.config cloudscribe.Core.Web.Integration\content /y
 
-xcopy ..\src\cloudscribe.WebHost\App_Start\* cloudscribe.Core.Web.Integration\content\App_Start /s /y /d
+::xcopy ..\src\cloudscribe.WebHost\Startup.cs cloudscribe.Core.Web.Integration\content /y
+call BatchSubstitute.cmd "cloudscribe.WebHost" "$rootnamespace$" ..\src\cloudscribe.WebHost\Startup.cs>cloudscribe.Core.Web.Integration\content\Startup.cs.pp
 
-xcopy ..\src\cloudscribe.WebHost\Controllers\* cloudscribe.Core.Web.Integration\content\Controllers /s /y /d
+echo on
+::ECHO.%ERRORLEVEL%
+
+::xcopy ..\src\cloudscribe.WebHost\Global.asax cloudscribe.Core.Web.Integration\content /y
+call BatchSubstitute.cmd "cloudscribe.WebHost" "$rootnamespace$" ..\src\cloudscribe.WebHost\Global.asax>cloudscribe.Core.Web.Integration\content\Global.asax.pp
+
+echo on
+::ECHO.%ERRORLEVEL%
+
+::xcopy ..\src\cloudscribe.WebHost\Global.asax.cs cloudscribe.Core.Web.Integration\content /y
+call BatchSubstitute.cmd "cloudscribe.WebHost" "$rootnamespace$" ..\src\cloudscribe.WebHost\Global.asax.cs>cloudscribe.Core.Web.Integration\content\Global.asax.cs.pp
+
+echo on
+::ECHO.%ERRORLEVEL%
+
+::xcopy ..\src\cloudscribe.WebHost\App_Start\* cloudscribe.Core.Web.Integration\content\App_Start /s /y /d
+
+call BatchSubstitute.cmd "cloudscribe.WebHost" "$rootnamespace$" ..\src\cloudscribe.WebHost\App_Start\BundleConfig.cs>cloudscribe.Core.Web.Integration\content\App_Start\BundleConfig.cs.pp
+
+echo on
+::ECHO.%ERRORLEVEL%
+
+call BatchSubstitute.cmd "cloudscribe.WebHost" "$rootnamespace$" ..\src\cloudscribe.WebHost\App_Start\FilterConfig.cs>cloudscribe.Core.Web.Integration\content\App_Start\FilterConfig.cs.pp
+
+echo on
+::ECHO.%ERRORLEVEL%
+
+call BatchSubstitute.cmd "cloudscribe.WebHost" "$rootnamespace$" ..\src\cloudscribe.WebHost\App_Start\RouteConfig.cs>cloudscribe.Core.Web.Integration\content\App_Start\RouteConfig.cs.pp
+
+echo on
+::ECHO.%ERRORLEVEL%
+
+call BatchSubstitute.cmd "cloudscribe.WebHost" "$rootnamespace$" ..\src\cloudscribe.WebHost\App_Start\Startup.Auth.cs>cloudscribe.Core.Web.Integration\content\App_Start\Startup.Auth.cs.pp
+
+echo on
+::ECHO.%ERRORLEVEL%
+
+call BatchSubstitute.cmd "cloudscribe.WebHost" "$rootnamespace$" ..\src\cloudscribe.WebHost\App_Start\WebApiConfig.cs>cloudscribe.Core.Web.Integration\content\App_Start\WebApiConfig.cs.pp
+
+echo on
+::ECHO.%ERRORLEVEL%
+
+::xcopy ..\src\cloudscribe.WebHost\Controllers\* cloudscribe.Core.Web.Integration\content\Controllers /s /y /d
+
+call BatchSubstitute.cmd "cloudscribe.WebHost" "$rootnamespace$" ..\src\cloudscribe.WebHost\Controllers\HomeController.cs>cloudscribe.Core.Web.Integration\content\Controllers\HomeController.cs.pp
+
+echo on
+::ECHO.%ERRORLEVEL%
 
 xcopy ..\src\cloudscribe.WebHost\Views\* cloudscribe.Core.Web.Integration\content\Views /s /y /d /EXCLUDE:cloudscribe.Core.Web.Integration.ExcludedViews.txt
 
-xcopy ..\src\cloudscribe.WebHost\DI\* cloudscribe.Core.Web.Integration\content\DI /s /y /d
+::xcopy ..\src\cloudscribe.WebHost\DI\* cloudscribe.Core.Web.Integration\content\DI /s /y /d
 ::copy ..\src\cloudscribe.WebHost\DI\Autofac\*.* cloudscribe.Core.Web.Integration\content\DI\Autofac
 
+call BatchSubstitute.cmd "cloudscribe.WebHost" "$rootnamespace$" ..\src\cloudscribe.WebHost\DI\CommonConventions.cs>cloudscribe.Core.Web.Integration\content\DI\CommonConventions.cs.pp
+
+echo on
+::ECHO.%ERRORLEVEL%
+
+call BatchSubstitute.cmd "cloudscribe.WebHost" "$rootnamespace$" ..\src\cloudscribe.WebHost\DI\Autofac\Modules\MvcModule.cs>cloudscribe.Core.Web.Integration\content\DI\Autofac\Modules\MvcModule.cs.pp
+
+echo on
+::ECHO.%ERRORLEVEL%
+
+call BatchSubstitute.cmd "cloudscribe.WebHost" "$rootnamespace$" ..\src\cloudscribe.WebHost\DI\Autofac\Modules\MvcSiteMapProviderModule.cs>cloudscribe.Core.Web.Integration\content\DI\Autofac\Modules\MvcSiteMapProviderModule.cs.pp
+
+echo on
+::ECHO.%ERRORLEVEL%
 
 SET pversion=%1
 IF NOT DEFINED pversion SET pversion="1.0.0-alpha0"
