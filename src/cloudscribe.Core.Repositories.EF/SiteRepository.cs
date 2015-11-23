6f6fcf9e9ae9830dc9fb75fe7c040303db6d5de6
Joe Audette <joeaudette@users.noreply.github.com>
2015-11-23 17:06:43 -0500

    wip on siterepository for ef7

--- src/cloudscribe.Core.Repositories.EF/SiteRepository.cs
+++ src/cloudscribe.Core.Repositories.EF/SiteRepository.cs
@@ -125,6 +125,178 @@ namespace cloudscribe.Core.Repositories.EF
             return await dbContext.Sites.CountAsync<SiteSettings>();
         }
 
+        public async Task<List<ISiteInfo>> GetList()
+        {
+            var query = from x in dbContext.Sites
+                        orderby x.SiteName ascending
+                        select new SiteInfo
+                        { SiteId = x.SiteId,
+                          SiteGuid = x.SiteGuid,
+                          IsServerAdminSite = x.IsServerAdminSite,
+                          PreferredHostName = x.PreferredHostName,
+                          SiteFolderName = x.SiteFolderName,
+                          SiteName = x.SiteName
+                        }
+                        ;
+
+            var items = await query.ToAsyncEnumerable<ISiteInfo>().ToList<ISiteInfo>();
+
+            //List<ISiteInfo> result = new List<ISiteInfo>(items); // will this work?
+
+            return items;
+        }
+
+        public async Task<int> CountOtherSites(int currentSiteId)
+        {
+            return await dbContext.Sites.CountAsync<SiteSettings>(x => x.SiteId != currentSiteId);
+        }
+
+        public async Task<List<ISiteInfo>> GetPageOtherSites(
+            int currentSiteId,
+            int pageNumber,
+            int pageSize)
+        {
+            int offset = (pageSize * pageNumber) - pageSize;
+
+            var query = from x in dbContext.Sites 
+                        .Skip(offset)
+                        .Take(pageSize)
+                        where (x.SiteId != currentSiteId)
+                        orderby x.SiteName ascending
+                        select new SiteInfo
+                        {
+                            SiteId = x.SiteId,
+                            SiteGuid = x.SiteGuid,
+                            IsServerAdminSite = x.IsServerAdminSite,
+                            PreferredHostName = x.PreferredHostName,
+                            SiteFolderName = x.SiteFolderName,
+                            SiteName = x.SiteName
+                        }
+                        ;
+
+            var items = await query.ToAsyncEnumerable<ISiteInfo>().ToList<ISiteInfo>();
+
+            return items;
+        }
+
+        public async Task<List<ISiteHost>> GetAllHosts()
+        {
+            var query = from x in dbContext.SiteHosts
+                        orderby x.HostName ascending
+                        select x;
+
+            var items = await query.ToAsyncEnumerable<ISiteHost>().ToList<ISiteHost>();
+
+            //List<ISiteHost> result = new List<ISiteHost>(items); // will this work?
+
+            return items;
+        }
+
+        public List<ISiteHost> GetAllHostsNonAsync()
+        {
+            var query = from x in dbContext.SiteHosts
+                        orderby x.HostName ascending
+                        select x;
+
+            var items = query.ToList<ISiteHost>();
+
+            //List<ISiteHost> result = new List<ISiteHost>(items); 
+
+            return items;
+        }
+
+        public async Task<int> GetHostCount()
+        {
+            return await dbContext.SiteHosts.CountAsync<SiteHost>();
+        }
+
+        public async Task<List<ISiteHost>> GetPageHosts(
+            int pageNumber,
+            int pageSize)
+        {
+            int offset = (pageSize * pageNumber) - pageSize;
+
+            var query = from x in dbContext.SiteHosts
+                        .Skip(offset)
+                        .Take(pageSize)
+                        orderby x.HostName ascending
+                        select x
+                        ;
+
+            var items = await query.ToAsyncEnumerable<ISiteHost>().ToList<ISiteHost>();
+
+            return items;
+        }
+
+        public async Task<List<ISiteHost>> GetSiteHosts(int siteId)
+        {
+            var query = from x in dbContext.SiteHosts
+                        where x.SiteId == siteId
+                        orderby x.HostName ascending
+                        select x
+                        ;
+
+            var items = await query.ToAsyncEnumerable<ISiteHost>().ToList<ISiteHost>();
+
+            return items;
+        }
+
+        public async Task<ISiteHost> GetSiteHost(string hostName)
+        {
+            var query = from x in dbContext.SiteHosts
+                        where x.HostName == hostName
+                        orderby x.HostName ascending
+                        select x
+                        ;
+
+            return await query.FirstOrDefaultAsync<SiteHost>();
+            
+        }
+
+        public async Task<bool> AddHost(Guid siteGuid, int siteId, string hostName)
+        {
+            SiteHost host = new SiteHost();
+            host.SiteGuid = siteGuid;
+            host.SiteId = siteId;
+            host.HostName = hostName;
+
+            dbContext.SiteHosts.Add(host);
+            int rowsAffected = await dbContext.SaveChangesAsync();
+
+            return rowsAffected > 0;
+
+        }
+
+        public async Task<bool> DeleteHost(int hostId)
+        {
+            var result = false;
+            var itemToRemove = await dbContext.SiteHosts.FirstAsync(x => x.HostId == hostId);
+            if (itemToRemove != null)
+            {
+                dbContext.SiteHosts.Remove(itemToRemove);
+                int rowsAffected = await dbContext.SaveChangesAsync();
+                result = rowsAffected > 0;
+            }
+
+            return result;
+
+        }
+
+        public async Task<int> GetSiteIdByHostName(string hostName)
+        {
+            var query = from x in dbContext.SiteHosts
+                        where x.HostName == hostName
+                        orderby x.HostName ascending
+                        select x
+                        ;
+
+            var host = await query.FirstAsync<SiteHost>();
+
+            if(host != null) { return host.SiteId; }
+
+            return -1; // not found
+        }
+
 
 
 
