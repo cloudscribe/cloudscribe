01197a2e17073d4ffb1f8760bfdcd5a5dfbfabdd
Joe Audette <joeaudette@users.noreply.github.com>
2016-04-26 09:07:21 -0400

    stub out project for NoDb core repositories
    this will be experimental to see how well NoDb can work

---
+++ src/cloudscribe.Core.Repositories.NoDb/SiteRepository.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Source Tree Solutions, LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Author:                  Joe Audette
+// Created:                 2016-04-26
+// Last Modified:           2016-04-26
+// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cloudscribe.Core.Repositories.NoDb
+{
+    public class SiteRepository
+    {
+    }
+}
