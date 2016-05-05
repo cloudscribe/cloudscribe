cb11268b96ec8b2f40d20be453f92c0cc9573b56
Joe Audette <joeaudette@users.noreply.github.com>
2016-05-05 14:24:00 -0400

    refactoring

--- src/cloudscribe.Core.Identity/SiteIdentityOptionsProvider.cs
+++ src/cloudscribe.Core.Identity/SiteIdentityOptionsProvider.cs
@@ -1,4 +1,9 @@
-﻿
+﻿// Copyright (c) Source Tree Solutions, LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Author:					Joe Audette/Derek Gray
+// Created:				    2016-05-04
+// Last Modified:		    2016-05-05
+// 
 
 using cloudscribe.Core.Models;
 using Microsoft.AspNet.Authentication.Cookies;
@@ -11,14 +16,18 @@ namespace cloudscribe.Core.Identity
     public class SiteIdentityOptionsProvider : IOptions<IdentityOptions>
     {
         private CookieAuthenticationEvents cookieEvents;
+        private SiteAuthCookieValidator siteValidator;
         private IHttpContextAccessor httpContextAccessor;
 
         public SiteIdentityOptionsProvider(
             IHttpContextAccessor httpContextAccessor,
-            CookieAuthenticationEvents cookieEvents)
+            CookieAuthenticationEvents cookieEvents,
+            SiteAuthCookieValidator siteValidator
+            )
         {
             this.httpContextAccessor = httpContextAccessor;
             this.cookieEvents = cookieEvents;
+            this.siteValidator = siteValidator;
         }
 
         public IdentityOptions Value
@@ -31,6 +40,8 @@ namespace cloudscribe.Core.Identity
                 var tenantPathBase = string.IsNullOrEmpty(tenant.SiteFolderName)
                     ? PathString.Empty
                     : new PathString("/" + tenant.SiteFolderName);
+
+                cookieEvents.OnValidatePrincipal = siteValidator.ValidatePrincipal;
 
                 var identityOptions = new IdentityOptions();
 
@@ -45,17 +56,16 @@ namespace cloudscribe.Core.Identity
                 identityOptions.User = singletonOptions.User;
                 */
 
+                Setup(identityOptions.Cookies.ApplicationCookie, AuthenticationScheme.Application, tenant);
                 Setup(identityOptions.Cookies.ExternalCookie, AuthenticationScheme.External, tenant);
                 Setup(identityOptions.Cookies.TwoFactorRememberMeCookie, AuthenticationScheme.TwoFactorRememberMe, tenant);
                 Setup(identityOptions.Cookies.TwoFactorUserIdCookie, AuthenticationScheme.TwoFactorUserId, tenant);
-                Setup(identityOptions.Cookies.ApplicationCookie, AuthenticationScheme.Application, tenant);
-
+                
                 var application = identityOptions.Cookies.ApplicationCookie;
 
                 application.AutomaticAuthenticate = true;
                 application.AutomaticChallenge = true;
-                application.CookiePath = "/" + tenant.SiteFolderName;
-
+                
                 return identityOptions;
             }
         }
@@ -70,6 +80,7 @@ namespace cloudscribe.Core.Identity
             options.AuthenticationScheme = $"{scheme}-{tenant.SiteFolderName}";
             options.LoginPath = tenantPathBase + "/account/login";
             options.LogoutPath = tenantPathBase + "/account/logoff";
+            options.CookiePath = "/" + tenant.SiteFolderName;
             options.Events = cookieEvents;
         }
     }
---
+++ src/cloudscribe.Core.Web/ServiceCollectionExtensions.cs
@@ -0,0 +1,74 @@
+﻿using cloudscribe.Core.Identity;
+using cloudscribe.Core.Models;
+using cloudscribe.Core.Web.Components;
+using cloudscribe.Core.Web.Components.Editor;
+using cloudscribe.Core.Web.Components.Messaging;
+using cloudscribe.Web.Common.Razor;
+using cloudscribe.Web.Pagination;
+using Microsoft.AspNet.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.OptionsModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cloudscribe.Core.Web
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCloudscribeCore(this IServiceCollection services, IConfigurationRoot configuration)
+        {
+            services.Configure<MultiTenantOptions>(configuration.GetSection("MultiTenantOptions"));
+            services.Configure<SiteConfigOptions>(configuration.GetSection("SiteConfigOptions"));
+            
+            services.Configure<UIOptions>(configuration.GetSection("UIOptions"));
+            services.Configure<CkeditorOptions>(configuration.GetSection("CkeditorOptions"));
+            
+
+            services.AddScoped<ITimeZoneResolver, RequestTimeZoneResolver>();
+            services.AddMultitenancy<SiteSettings, CachingSiteResolver>();
+            services.AddSingleton<IOptions<IdentityOptions>, SiteIdentityOptionsProvider>();
+
+            services.AddScoped<SiteManager, SiteManager>();
+            
+            services.AddScoped<GeoDataManager, GeoDataManager>();
+            services.AddScoped<SystemInfoManager, SystemInfoManager>();
+            services.AddScoped<IpAddressTracker, IpAddressTracker>();
+
+            services.AddScoped<SiteDataProtector>();
+
+            
+
+            services.AddIdentity<SiteUser, SiteRole>()
+                .AddUserStore<UserStore<SiteUser>>()
+                .AddRoleStore<RoleStore<SiteRole>>()
+                .AddUserManager<SiteUserManager<SiteUser>>()
+                .AddRoleManager<SiteRoleManager<SiteRole>>();
+
+            services.AddSingleton<IOptions<IdentityOptions>, SiteIdentityOptionsProvider>();
+
+            services.AddScoped<IUserClaimsPrincipalFactory<SiteUser>, SiteUserClaimsPrincipalFactory<SiteUser, SiteRole>>();
+            services.AddScoped<IPasswordHasher<SiteUser>, SitePasswordHasher<SiteUser>>();
+            services.AddScoped<SiteSignInManager<SiteUser>, SiteSignInManager<SiteUser>>();
+            services.AddScoped<SiteAuthCookieValidator, SiteAuthCookieValidator>();
+            services.AddScoped<SiteCookieAuthenticationEvents, SiteCookieAuthenticationEvents>();
+
+
+
+            services.AddTransient<IBuildPaginationLinks, PaginationLinkBuilder>();
+
+            services.AddTransient<IEmailTemplateService, HardCodedEmailTemplateService>();
+            services.AddTransient<ISiteMessageEmailSender, SiteEmailMessageSender>();
+            services.AddTransient<ISmsSender, SiteSmsSender>();
+
+            services.AddSingleton<IThemeListBuilder, SiteThemeListBuilder>();
+
+
+
+
+            return services;
+        }
+    }
+}
