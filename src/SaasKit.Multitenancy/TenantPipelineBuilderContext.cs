edf894c83b5b9f7aeae3c218e71d7efae1862af5
Joe Audette <joeaudette@users.noreply.github.com>
2016-06-29 06:06:45 -0400

    change back to nuget for saaskit

--- src/SaasKit.Multitenancy/TenantPipelineBuilderContext.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SaasKit.Multitenancy
-{
-    public class TenantPipelineBuilderContext<TTenant>
-    {
-        public TenantContext<TTenant> TenantContext { get; set; }
-        public TTenant Tenant { get; set; }
-    }
-}
