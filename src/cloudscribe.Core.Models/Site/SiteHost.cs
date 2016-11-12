4b358f05dec0a39c5a03d6cd0253097f713db3c6
Joe Audette <joeaudette@users.noreply.github.com>
2016-11-12 10:02:08 -0500

    fix errors when adding host names

--- src/cloudscribe.Core.Models/Site/SiteHost.cs
+++ src/cloudscribe.Core.Models/Site/SiteHost.cs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Author:					Joe Audette
 // Created:					2014-08-17
-// Last Modified:			2016-05-14
+// Last Modified:			2016-11-12
 // 
 
 using System;
@@ -11,6 +11,10 @@ namespace cloudscribe.Core.Models
 {
     public class SiteHost : ISiteHost
     {
+        public SiteHost()
+        {
+            Id = Guid.NewGuid();
+        }
         public Guid Id { get; set; }
 
         private string hostName = string.Empty;
