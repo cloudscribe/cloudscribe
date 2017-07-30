ee1cb135277ef094fd46751699851d4efde38928
Joe Audette <joe.audette@gmail.com>
2017-07-30 09:45:09 -0400

    fix mysql migrations

--- src/cloudscribe.Core.Storage.EFCore.MySql/Migrations/20170526173128_changes20170526.cs
+++ src/cloudscribe.Core.Storage.EFCore.MySql/Migrations/20170526173128_changes20170526.cs
@@ -11,8 +11,7 @@ namespace cloudscribe.Core.Storage.EFCore.MySql.Migrations
             migrationBuilder.AddColumn<DateTime>(
                 name: "TermsUpdatedUtc",
                 table: "cs_Site",
-                nullable: false,
-                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+                nullable: false);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
--- src/cloudscribe.Core.Storage.EFCore.MySql/Startup.cs
+++ src/cloudscribe.Core.Storage.EFCore.MySql/Startup.cs
@@ -27,6 +27,7 @@ namespace cloudscribe.Core.Storage.EFCore.MySql
             var builder = new DbContextOptionsBuilder<CoreDbContext>();
             builder.UseMySql("Server=yourserver;Database=yourdb;Uid=youruser;Pwd=yourpassword;Charset=utf8;");
             
+
             return new CoreDbContext(builder.Options);
         }
     }
