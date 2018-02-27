d4d6383ec08ea36c3854b039ef8dd331a01e34d8
Joe Audette <joe.audette@gmail.com>
2018-02-27 15:46:18 -0500

    wip refactoring email

---
+++ src/cloudscribe.Messaging.Email/IMessageSender.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+
+namespace cloudscribe.Messaging.Email
+{
+    public interface IMessageSender
+    {
+        string Name { get; }
+        Task SendEmailAsync(
+            string toEmailCsv,
+            string fromEmail,
+            string subject,
+            string plainTextMessage,
+            string htmlMessage,
+            string replyToEmail = null,
+            Importance importance = Importance.Normal,
+            bool isTransactional = true,
+            string fromName = null,
+            string replyToName = null,
+            string toAliasCsv = null,
+            string ccEmailCsv = null,
+            string ccAliasCsv = null,
+            string bccEmailCsv = null,
+            string bccAliasCsv = null,
+            string[] attachmentFilePaths = null
+            );
+
+    }
+}
