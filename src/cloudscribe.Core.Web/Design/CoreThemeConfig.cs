16a3e89ef06c57df6da51529e7cf55c7abf7ca9b
Joe Audette <joe.audette@gmail.com>
2018-04-19 12:10:48 -0400

    make per theme config possible
    for use in bootstrap4

---
+++ src/cloudscribe.Core.Web/Design/CoreThemeConfig.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace cloudscribe.Core.Web.Design
+{
+    public class CoreThemeConfig
+    {
+        public CoreThemeConfig()
+        {
+            ThemeSettings = new List<CoreThemeSettings>();
+
+        }
+
+        public string DefaultTheme { get; set; } = "default";
+
+        public List<CoreThemeSettings> ThemeSettings { get; set; }
+
+        public CoreThemeSettings GetThemSettings(string themeSettingsId)
+        {
+            foreach(var theme in ThemeSettings)
+            {
+                if(theme.ThemeName == themeSettingsId)
+                {
+                    return theme;
+                }
+
+            }
+
+            foreach (var theme in ThemeSettings)
+            {
+                if (theme.ThemeName == DefaultTheme)
+                {
+                    return theme;
+                }
+
+            }
+
+            return new CoreThemeSettings();
+
+        }
+    }
+}
