d2e972b8253e3e4b206a06b71d00ab4bdf112cce
Joe Audette <joe.audette@gmail.com>
2018-04-24 16:19:45 -0400

    wip on bootstrap4

---
+++ src/cloudscribe.Core.Web.Views.Bootstrap4/StartupExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.FileProviders;
+using System.Reflection;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class StartupExtensions
+    {
+        public static RazorViewEngineOptions AddCloudscribeCoreBootstrap4Views(this RazorViewEngineOptions options)
+        {
+            options.FileProviders.Add(new EmbeddedFileProvider(
+                    typeof(StartupExtensions).GetTypeInfo().Assembly,
+                    "cloudscribe.Core.Web.Views.Bootstrap4"
+                ));
+
+            return options;
+        }
+    }
+}
