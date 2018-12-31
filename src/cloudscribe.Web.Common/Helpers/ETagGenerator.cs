0e9238207eb609e2868059cee8a58718be9b7aba
Joe Audette <joe.audette@gmail.com>
2018-12-31 07:08:01 -0500

    tidy

--- src/cloudscribe.Web.Common/Helpers/ETagGenerator.cs
+++ src/cloudscribe.Web.Common/Helpers/ETagGenerator.cs
@@ -11,31 +11,7 @@ namespace cloudscribe.Web.Common.Helpers
 {
     public static class ETagGenerator
     {
-        public static string GetETag(string key, byte[] contentBytes)
-        {
-            var keyBytes = Encoding.UTF8.GetBytes(key);
-            var combinedBytes = Combine(keyBytes, contentBytes);
-
-            return GenerateETag(combinedBytes);
-        }
-
-        private static string GenerateETag(byte[] data)
-        {
-            using (var md5 = MD5.Create())
-            {
-                var hash = md5.ComputeHash(data);
-                string hex = BitConverter.ToString(hash);
-                return hex.Replace("-", "");
-            }
-        }
-
-        private static byte[] Combine(byte[] a, byte[] b)
-        {
-            byte[] c = new byte[a.Length + b.Length];
-            Buffer.BlockCopy(a, 0, c, 0, a.Length);
-            Buffer.BlockCopy(b, 0, c, a.Length, b.Length);
-            return c;
-        }
+        
 
         public static StatusCodeResult AddEtagForStream(HttpContext context, Stream stream)
         {
@@ -63,5 +39,31 @@ namespace cloudscribe.Web.Common.Helpers
 
             return checksum;
         }
+
+        //public static string GetETag(string key, byte[] contentBytes)
+        //{
+        //    var keyBytes = Encoding.UTF8.GetBytes(key);
+        //    var combinedBytes = Combine(keyBytes, contentBytes);
+
+        //    return GenerateETag(combinedBytes);
+        //}
+
+        //private static string GenerateETag(byte[] data)
+        //{
+        //    using (var md5 = MD5.Create())
+        //    {
+        //        var hash = md5.ComputeHash(data);
+        //        string hex = BitConverter.ToString(hash);
+        //        return hex.Replace("-", "");
+        //    }
+        //}
+
+        //private static byte[] Combine(byte[] a, byte[] b)
+        //{
+        //    byte[] c = new byte[a.Length + b.Length];
+        //    Buffer.BlockCopy(a, 0, c, 0, a.Length);
+        //    Buffer.BlockCopy(b, 0, c, a.Length, b.Length);
+        //    return c;
+        //}
     }
 }
