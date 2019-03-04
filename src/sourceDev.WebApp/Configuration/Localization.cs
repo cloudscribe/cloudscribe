f2eb14befd410b417adc3110b9a961f605a53729
Joe Audette <joeaudette@users.noreply.github.com>
2019-03-04 07:10:19 -0500

    localization options

--- src/sourceDev.WebApp/Configuration/Localization.cs
+++ src/sourceDev.WebApp/Configuration/Localization.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using cloudscribe.Core.Web.Localization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Localization;
 using System.Globalization;
+using System.Linq;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -18,18 +20,22 @@ namespace Microsoft.Extensions.DependencyInjection
 
             services.AddLocalization(options => options.ResourcesPath = "GlobalResources");
 
-            services.Configure<RequestLocalizationOptions>(options =>
-            {
-                var supportedCultures = new[]
+            var supportedCultures = new[]
                 {
                     new CultureInfo("en-US"),
-                    new CultureInfo("en-GB"),
+                    //new CultureInfo("en-GB"),
                     new CultureInfo("cy-GB"),
-                    new CultureInfo("cy"),
-                    new CultureInfo("sv"),
+                   // new CultureInfo("cy"),
+                   // new CultureInfo("sv"),
                     new CultureInfo("fr-FR"),
-                    new CultureInfo("fr")
+                    //new CultureInfo("fr")
                 };
+
+            var routeSegmentLocalizationProvider = new UrlSegmentRequestCultureProvider(supportedCultures.ToList());
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                
 
                 // State what the default culture for your application is. This will be used if no specific culture
                 // can be determined for a given request.
@@ -54,6 +60,9 @@ namespace Microsoft.Extensions.DependencyInjection
                 //  // My custom request culture logic
                 //  return new ProviderCultureResult("en");
                 //}));
+
+                options.RequestCultureProviders.Insert(0, routeSegmentLocalizationProvider);
+
             });
 
             return services;
