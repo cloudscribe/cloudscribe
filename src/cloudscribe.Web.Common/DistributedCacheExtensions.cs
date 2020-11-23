8fa56795fd53f522b9cece2e7fc68cb0f32ca1df
Jim Kerslake <JimK@smallbusiness.local>
2020-11-23 13:46:06 +0000

    Update ref to web.navigation - removing autoformatter usage

--- src/cloudscribe.Web.Common/DistributedCacheExtensions.cs
+++ src/cloudscribe.Web.Common/DistributedCacheExtensions.cs
@@ -1,32 +1,32 @@
 ﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 using System.Threading;
 using System.Threading.Tasks;
-using cloudscribe.Web.Common.Serialization;
 
 namespace cloudscribe.Web.Common
 {
     public static class DistributedCacheExtensions
     {
-        
-        public async static Task SetAsync<T>(
-            this IDistributedCache distributedCache, 
-            string key, 
-            T value, 
-            DistributedCacheEntryOptions options, 
-            CancellationToken token = default(CancellationToken))
+        public async static Task SetAsync<T>(this IDistributedCache distributedCache,
+                                             string key,
+                                             T value,
+                                             DistributedCacheEntryOptions options,
+                                             CancellationToken token = default(CancellationToken))
         {
-            await distributedCache.SetAsync(key, value.ToByteArray(), options, token);
+            var json = JsonConvert.SerializeObject(value);
+
+            await distributedCache.SetStringAsync(key, json, options, token);
         }
 
-        public async static Task<T> GetAsync<T>(
-            this IDistributedCache distributedCache, 
-            string key, 
-            CancellationToken token = default(CancellationToken)) where T : class
-        {
-            var result = await distributedCache.GetAsync(key, token);
-            return result.FromByteArray<T>();
-        }
-        
 
+        public async static Task<T> GetAsync<T>(this IDistributedCache distributedCache,
+                                                string key,
+                                                CancellationToken token = default(CancellationToken))
+                                                where T : class
+        {
+            var json = await distributedCache.GetStringAsync(key);
+
+            return json != null ? JsonConvert.DeserializeObject<T>(json) : null;
+        }
     }
 }
--- src/cloudscribe.Web.Common/Serialization/Extensions.cs
+++ src/cloudscribe.Web.Common/Serialization/Extensions.cs
@@ -282,32 +282,32 @@ namespace cloudscribe.Web.Common.Serialization
             return dateString;
         }
 
-        public static byte[] ToByteArray(this object obj)
-        {
-            if (obj == null)
-            {
-                return null;
-            }
-            BinaryFormatter binaryFormatter = new BinaryFormatter();
-            using (MemoryStream memoryStream = new MemoryStream())
-            {
-                binaryFormatter.Serialize(memoryStream, obj);
-                return memoryStream.ToArray();
-            }
-        }
+        //public static byte[] ToByteArray(this object obj)
+        //{
+        //    if (obj == null)
+        //    {
+        //        return null;
+        //    }
+        //    BinaryFormatter binaryFormatter = new BinaryFormatter();
+        //    using (MemoryStream memoryStream = new MemoryStream())
+        //    {
+        //        binaryFormatter.Serialize(memoryStream, obj);
+        //        return memoryStream.ToArray();
+        //    }
+        //}
 
-        public static T FromByteArray<T>(this byte[] byteArray) where T : class
-        {
-            if (byteArray == null)
-            {
-                return default(T);
-            }
-            BinaryFormatter binaryFormatter = new BinaryFormatter();
-            using (MemoryStream memoryStream = new MemoryStream(byteArray))
-            {
-                return binaryFormatter.Deserialize(memoryStream) as T;
-            }
-        }
+        //public static T FromByteArray<T>(this byte[] byteArray) where T : class
+        //{
+        //    if (byteArray == null)
+        //    {
+        //        return default(T);
+        //    }
+        //    BinaryFormatter binaryFormatter = new BinaryFormatter();
+        //    using (MemoryStream memoryStream = new MemoryStream(byteArray))
+        //    {
+        //        return binaryFormatter.Deserialize(memoryStream) as T;
+        //    }
+        //}
 
     }
 }
