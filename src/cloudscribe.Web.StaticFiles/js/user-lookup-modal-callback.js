0948fafce47a0daecc4a66166a32b23cb35cb8ad
Jim Kerslake <39943820+JimKerslake@users.noreply.github.com>
2024-05-31 15:23:50 +0100

    remove couple of onClicks

--- src/cloudscribe.Web.StaticFiles/js/user-lookup-modal-callback.js
+++ src/cloudscribe.Web.StaticFiles/js/user-lookup-modal-callback.js
@@ -1,4 +1,23 @@
-﻿function userSelected(ele) {
+﻿$(function () {
+    // get rid of onClick references in the BS5 views
+    var ele = document.getElementById("btnUserSelected");
+    if (ele != null & ele != undefined) { 
+        if (window.UserSelectedCallback) {
+            var userInfo = {
+                "id": ele.dataset.userId,
+                "email": ele.dataset.userEmail,
+                "displayName": ele.dataset.userDisplayName,
+                "firstName": ele.dataset.userFirstName,
+                "lastName": ele.dataset.userLastName
+            };
+            window.UserSelectedCallback(userInfo);
+        }
+        $('#userLookupModal').closest(".modal").modal("hide");
+    }
+})();
+
+
+function userSelected(ele) {
     if (window.UserSelectedCallback) {
         var userInfo = {
             "id": ele.dataset.userId,
@@ -11,3 +30,5 @@
     }
     $('#userLookupModal').closest(".modal").modal("hide");
 }
+
+
