5eafa323e6b5adeefa6bde46210e531fb4bb5cdd
Jim Kerslake <39943820+JimKerslake@users.noreply.github.com>
2024-07-18 17:11:30 +0100

    upgrade revert attempt to remove onclick

--- src/cloudscribe.Web.StaticFiles/js/user-lookup-modal-callback.js
+++ src/cloudscribe.Web.StaticFiles/js/user-lookup-modal-callback.js
@@ -1,20 +1,24 @@
-﻿$(function () {
-    // get rid of onClick references in the BS5 views
-    var ele = document.getElementById("btnUserSelected");
-    if (ele != null & ele != undefined) { 
-        if (window.UserSelectedCallback) {
-            var userInfo = {
-                "id": ele.dataset.userId,
-                "email": ele.dataset.userEmail,
-                "displayName": ele.dataset.userDisplayName,
-                "firstName": ele.dataset.userFirstName,
-                "lastName": ele.dataset.userLastName
-            };
-            window.UserSelectedCallback(userInfo);
-        }
-        $('#userLookupModal').closest(".modal").modal("hide");
-    }
-})();
+﻿//$(function () {
+//    // get rid of onClick references in the BS5 views
+//    // no this isn't working and too obscure to trace its usages across everything right now - jk
+//    var elements = $(".btn-modal-userselect");
+
+//    elements.each(function () {
+//        $(this).on('click', function() {
+//            if (window.UserSelectedCallback) {
+//                var userInfo = {
+//                    "id": $(this).data.userId,
+//                    "email": $(this).data.userEmail,
+//                    "displayName": $(this).data.userDisplayName,
+//                    "firstName": $(this).data.userFirstName,
+//                    "lastName": $(this).data.userLastName
+//                };
+//                window.UserSelectedCallback(userInfo);
+//            }
+//            $('#userLookupModal').closest(".modal").modal("hide");
+//        })
+//    })
+//});
 
 
 function userSelected(ele) {
