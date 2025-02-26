2438a3c4ecb70c54435458fe981fc1a36394980e
Mark Appleton <mark.appleton@idoxgroup.com>
2025-02-26 10:28:24 +0000

    enabling drag & drop on Summernote. Fixing FileManager Controller to actually take notice of the file drag and drop url specified by the user

--- src/cloudscribe.FileManager.Web/Controllers/FileManagerController.cs
+++ src/cloudscribe.FileManager.Web/Controllers/FileManagerController.cs
@@ -257,7 +257,7 @@ namespace cloudscribe.FileManager.Web.Controllers
             string newFileName = string.Empty;
             var allowRootPath = false;
             var createThumbnail = false;
-            var requestedFilePath = Request.Form["targetPath"].ToString();
+            var requestedFilePath = HttpContext.Request.Form["uploadUrl"].ToString();
 
             //TODO: refactor, this is very cloudscribe core specific
             if(requestedFilePath == "/media/user-avatars")
